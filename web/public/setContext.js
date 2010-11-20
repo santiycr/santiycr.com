fc96da5aa3735e430b547a7d786e7acf9e4b2fca
santi <santiycr@gmail.com>
2010-11-19 23:05:41 -0800

    user extension to use in sauce

---
+++ web/public/setContext.js
@@ -0,0 +1,44 @@
+window.contextTimeout = null;
+
+function fade(div) {
+    var alpha = div.style.opacity;
+    var new_alpha = alpha - 0.1;
+    div.style.opacity = new_alpha;
+    if (new_alpha){
+        window.contextTimeout = setTimeout(function(){fade(div);}, 50);
+    } else {
+        this.browserbot.getUserWindow().document.body.removeChild(div);
+    }
+}
+
+Selenium.prototype.doSetContext = function(context) {
+//function setContext(context){
+   var ctx = this.browserbot.getUserWindow().document.getElementById("context");
+   var newdiv;
+   if (ctx) {
+       newdiv = ctx;
+       clearTimeout(window.contextTimeout);
+   } else {
+       newdiv = this.browserbot.getUserWindow().document.createElement("div");
+   }
+   newdiv.setAttribute('id', "context");
+   newdiv.style.zIndex = "500";
+   newdiv.style.position = "fixed";
+   newdiv.style.left = "50%";
+   newdiv.style.top = "10px";
+   newdiv.style.width = "500px";
+   newdiv.style.maxHeight = "100px";
+   newdiv.style.overflow = "hidden";
+   newdiv.style.margin = "0 0 0 -250px";
+   newdiv.style.padding = "5px";
+   newdiv.style.border = "1px solid #333";
+   newdiv.style.MozBorderRadius = "5px";
+   newdiv.style.borderRadius = "5px";
+   newdiv.style.textAlign = "center";
+   newdiv.style.font = "normal bold 20px/25px Arial,sans-serif";
+   newdiv.style.background = "#EEE";
+   newdiv.style.opacity = 0.8;
+   newdiv.innerHTML = context;   
+   this.browserbot.getUserWindow().document.body.appendChild(newdiv);
+   window.contextTimeout = setTimeout(function(){fade(newdiv);}, 2000);
+}
