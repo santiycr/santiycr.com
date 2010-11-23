4bf8eba9ae8d0646cc1d230f5e79b3d759e8a3a8
santi <santiycr@gmail.com>
2010-11-23 15:13:03 -0800

    style fixes

--- web/public/setContext.js
+++ web/public/setContext.js
@@ -1,44 +1,42 @@
 window.contextTimeout = null;
 
-function fade(div) {
-    var alpha = div.style.opacity;
+function fade(ctx) {
+    var alpha = ctx.style.opacity;
     var new_alpha = alpha - 0.1;
-    div.style.opacity = new_alpha;
+    ctx.style.opacity = new_alpha;
     if (new_alpha){
-        window.contextTimeout = setTimeout(function(){fade(div);}, 50);
+        window.contextTimeout = setTimeout(function(){fade(ctx);}, 50);
     } else {
-        this.browserbot.getUserWindow().document.body.removeChild(div);
+        this.browserbot.getUserWindow().document.body.removeChild(ctx);
     }
 }
 
 Selenium.prototype.doSetContext = function(context) {
-   var ctx = this.browserbot.getUserWindow().document.getElementById("context");
-   var newdiv;
+   var ctx = this.browserbot.getUserWindow().document.getElementById("selenium-context");
    if (ctx) {
-       newdiv = ctx;
        clearTimeout(window.contextTimeout);
    } else {
-       newdiv = this.browserbot.getUserWindow().document.createElement("div");
+       ctx = this.browserbot.getUserWindow().document.createElement("div");
    }
-   newdiv.setAttribute('id', "context");
-   newdiv.style.zIndex = "500";
-   newdiv.style.position = "fixed";
-   newdiv.style.left = "50%";
-   newdiv.style.top = "10px";
-   newdiv.style.width = "500px";
-   newdiv.style.maxHeight = "100px";
-   newdiv.style.overflow = "hidden";
-   newdiv.style.margin = "0 0 0 -250px";
-   newdiv.style.padding = "5px";
-   newdiv.style.MozBorderRadius = "8px";
-   newdiv.style.borderRadius = "8px";
-   newdiv.style.textAlign = "center";
-   newdiv.style.font = "normal normal 20px/25px Arial,sans-serif";
-   newdiv.style.background = "#000";
-   newdiv.style.border = "1px solid #CCC";
-   newdiv.style.color = "#EEE";
-   newdiv.style.opacity = 0.8;
-   newdiv.innerHTML = context;   
-   this.browserbot.getUserWindow().document.body.appendChild(newdiv);
-   window.contextTimeout = setTimeout(function(){fade(newdiv);}, 3000);
+   ctx.setAttribute('id', "selenium-context");
+   ctx.style.zIndex = "500";
+   ctx.style.position = "fixed";
+   ctx.style.left = "50%";
+   ctx.style.top = "10px";
+   ctx.style.width = "500px";
+   ctx.style.maxHeight = "100px";
+   ctx.style.overflow = "hidden";
+   ctx.style.margin = "0 0 0 -250px";
+   ctx.style.padding = "5px";
+   ctx.style.MozBorderRadius = "8px";
+   ctx.style.borderRadius = "8px";
+   ctx.style.textAlign = "center";
+   ctx.style.font = "normal normal 20px/25px Arial,sans-serif";
+   ctx.style.background = "#000";
+   ctx.style.border = "1px solid #CCC";
+   ctx.style.color = "#EEE";
+   ctx.style.opacity = 0.8;
+   ctx.innerHTML = context;   
+   this.browserbot.getUserWindow().document.body.appendChild(ctx);
+   window.contextTimeout = setTimeout(function(){fade(ctx);}, 3000);
 }
